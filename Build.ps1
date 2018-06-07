d1ab7a5947fe8e666e979686a3f80e056b19eb1d
Mark Gould <markg@spindlemedia.com>
2018-06-07 11:13:39 -0500

    Change fx version to 2.1.0

--- Build.ps1
+++ Build.ps1
@@ -43,7 +43,7 @@ exec { & dotnet build -c Release --version-suffix=$buildSuffix }
 Push-Location -Path .\ContosoUniversity.IntegrationTests
 
 try {
-	exec { & dotnet xunit -configuration Release -nobuild --fx-version 2.0.0 }
+	exec { & dotnet xunit -configuration Release -nobuild --fx-version 2.1.0 }
 }
 finally {
 	Pop-Location
